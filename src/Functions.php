4bf16a990b9f858a582a2c6a7ab7bd817cefd10a
mrokumus <okumusmr@gmail.com>
2022-05-25 11:28:08 +0300

    Update Functions.php
    preg_replace null error solved

--- src/Functions.php
+++ src/Functions.php
@@ -95,7 +95,10 @@ class Functions
 	 */
 	public function cleanerTextarea($text): ?string
 	{
-		return trim(str_replace(['insert', 'update', 'union', 'select', '*', '<script'], ['', '', '', '', '', ''], $text));
+		if ($text !== NULL) {
+			return trim(str_replace(['insert', 'update', 'union', 'select', '*', '<script'], ['', '', '', '', '', ''], $text));
+		}
+		return NULL;
 	}
 
 	/**
@@ -526,7 +529,7 @@ class Functions
 		$result = null;
 		if (strlen($text) > $limit) {
 			$result .= $this->kisalt($text, $limit);
-			$uniq = uniqid('un-',true) . time();
+			$uniq = uniqid('un-', true) . time();
 			$result .= '  <i class="' . $icon . ' ml-2 table-modal-icon" data-toggle="modal" data-target="#page_modal_' . $uniq . '"></i>';
 			$result .= '<div id="page_modal_' . $uniq . '" class="modal fade" tabindex="-1">
                                 <div class="modal-dialog  modal-lg modal-dialog-centered modal-dialog-scrollable">
