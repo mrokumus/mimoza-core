104ec6943a11bbfa1d0e3a54104f86f003fc1230
Muhammed Raşit OKUMUŞ <okumusmr@gmail.com>
2022-06-11 14:49:55 +0300

    Update Log.php
    this() function added. logThis() function will be deprecated sooon

--- src/Log.php
+++ src/Log.php
@@ -18,10 +18,58 @@ class Log
 
 	}
 
+	/**
+	 * @param string $logType
+	 * @param string $detail
+	 * @return void
+	 */
+	public function this(string $logType, string $detail = ""): void
+	{
+		if (!isset($this->logTypes[$logType])) {
+			$this->addLogType($logType);
+		}
+
+		if ($this->checkLogType($logType)) {
+			//sunucuda php_browscap.ini aktif ise daha okunaklı browser bilgisi alınabilir.
+			if (function_exists('get_browser')) {
+				$browser = array();
+				$browser["parent"] = $this->getBrowser();
+				$browser["platform"] = $this->getOs();
+			} else {
+				$browser['parent'] = "";
+				$browser['platform'] = "";
+			}
+
+			$user_id = isset($_SESSION['user_id']) ? (int)($_SESSION['user_id']) : null;
+			$user_ip = $this->getUserIP();
+			$query = $this->database::$db->prepare('INSERT INTO logs (user_id, log_datetime, client_ip, log_type, log_page, log_query_string, log_detail,log_browser,log_os,log_http_user_agent) VALUES (:user_id, NOW(), :client_ip, :log_type, :log_page, :log_query_string, :log_detail,:log_browser,:log_os,:log_http_user_agent)');
+
+			$query->bindParam(':user_id', $user_id);
+			$query->bindParam(':client_ip', $user_ip);
+			$query->bindParam(':log_type', $logType);
+			$query->bindParam(':log_page', $_SERVER['REQUEST_URI']);
+			$query->bindParam(':log_query_string', $_SERVER['QUERY_STRING']);
+			$query->bindParam(':log_http_user_agent', $_SERVER['HTTP_USER_AGENT']);
+			$query->bindParam(':log_detail', $detail);
+			$query->bindParam(':log_browser', $browser['parent']);
+			$query->bindParam(':log_os', $browser['platform']);
+			$query->execute();
+		} else {
+			die("logtype bulunmuyor. ekleyiniz. #" . $logType);
+		}
+
+	}
+
+	/**
+	 * This function will be deprecated
+	 *
+	 * @param string $logType
+	 * @param string $detail
+	 * @return void
+	 */
 	public function logThis(string $logType, string $detail = ""): void
 	{
 
-		$db_connection = $this->database::$db;
 		if ($this->checkLogType($logType)) {
 
 			//sunucuda php_browscap.ini aktif ise daha okunaklı browser bilgisi alınabilir.
@@ -36,7 +84,7 @@ class Log
 
 			$user_id = isset($_SESSION['user_id']) ? (int)($_SESSION['user_id']) : null;
 			$user_ip = $this->getUserIP();
-			$query = $db_connection->prepare('INSERT INTO logs (user_id, log_datetime, client_ip, log_type, log_page, log_query_string, log_detail,log_browser,log_os,log_http_user_agent) VALUES (:user_id, NOW(), :client_ip, :log_type, :log_page, :log_query_string, :log_detail,:log_browser,:log_os,:log_http_user_agent)');
+			$query = $this->database::$db->prepare('INSERT INTO logs (user_id, log_datetime, client_ip, log_type, log_page, log_query_string, log_detail,log_browser,log_os,log_http_user_agent) VALUES (:user_id, NOW(), :client_ip, :log_type, :log_page, :log_query_string, :log_detail,:log_browser,:log_os,:log_http_user_agent)');
 
 			$query->bindParam(':user_id', $user_id);
 			$query->bindParam(':client_ip', $user_ip);
@@ -56,8 +104,7 @@ class Log
 
 	public function getLogTypes()
 	{
-		$db_connection = $this->database::$db;
-		$query = $db_connection->query('SELECT * FROM log_types');
+		$query = $this->database::$db->query('SELECT * FROM log_types');
 		$query->execute();
 		$logs = $query->fetchAll(PDO::FETCH_ASSOC);
 		if ($query->rowCount() > 0) {
@@ -68,6 +115,40 @@ class Log
 		}
 		return false;
 
+
+	}
+
+	private function addLogType(string $logValue): bool
+	{
+		$lastValueQuery = $this->database::$db->query('SELECT * FROM log_types ORDER BY log_val DESC LIMIT 1');
+		$lastValue = $lastValueQuery->fetch(PDO::FETCH_OBJ);
+		$uniqueInt = (int)$lastValue->log_val + 1;
+
+		$insert = $this->database::insert('log_types', [
+			'log_key' => $logValue,
+			'log_val' => $uniqueInt,
+			'log_desc' => 'Auto added type'
+		]);
+
+		if ($insert) {
+			return true;
+		}
+		return false;
+	}
+
+	private function checkLogType(string $logValue): bool
+	{
+		$dbLogValue = $this->database::selectQuery('log_types', ['log_key' => $logValue], true);
+		if (!empty($dbLogValue->log_key)) {
+			return true;
+		}
+
+		$result = $this->addLogType($logValue);
+
+		if ($result) {
+			return true;
+		}
+		return false;
 
 	}
 
@@ -133,34 +214,6 @@ class Log
 		}
 
 		return $browser;
-	}
-
-	private function checkLogType(string $log_val): bool
-	{
-		$db_connection = $this->database;
-		$query = $db_connection::$db->prepare('SELECT * FROM log_types WHERE log_val=:log_val');
-		$query->bindParam(':log_val', $log_val);
-		$query->execute();
-		$row = $query->fetch(PDO::FETCH_ASSOC);
-
-		if ($query->rowCount() > 0) {
-			return true;
-		}
-
-		$lastValueQuery = $db_connection::$db->query('SELECT log_val FROM log_types ORDER BY log_val DESC LIMIT 1');
-		$lastValue = $lastValueQuery->fetch(PDO::FETCH_ASSOC);
-		$uniqueInt = (int)$lastValue['log_val'] + 1;
-
-		$insert = $db_connection::insert('log_types', [
-			'log_key' => $log_val,
-			'log_val' => $uniqueInt,
-			'log_desc' => 'Auto added type'
-		]);
-		if ($insert) {
-			return true;
-		}
-		return false;
-
 	}
 
 	private function getUserIP()
